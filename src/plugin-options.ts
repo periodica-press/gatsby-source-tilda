f9103612ca28936ae1b1ad05a71868e6627038da
Gamza Ramazanov <84990031+Gamza222@users.noreply.github.com>
2023-05-23 15:26:30 +0300

    feat: added some interfaces and types

--- src/plugin-options.ts
+++ src/plugin-options.ts
@@ -1,13 +1,19 @@
 import TildaApi from './api/tilda';
+import { PluginConfigProps } from './fetch-data';
+
+export interface PluginOptionsProps {
+  secret: any;
+  publicKey: any;
+}
 
 const defaultOptions = { exclude: [] };
 
-export const createPluginConfig = (pluginOptions) => {
+export const createPluginConfig : (arg0: PluginOptionsProps) => PluginConfigProps = (pluginOptions: PluginOptionsProps) => {
   const { secret, publicKey } = pluginOptions;
   const conf = { ...defaultOptions, ...pluginOptions };
 
   return {
-    get: (key) => conf[key],
+    get: (key: string) => conf[key as keyof typeof conf],
     api: new TildaApi(publicKey, secret),
     getOriginalPluginOptions: () => pluginOptions,
     exclude: conf.exclude,
